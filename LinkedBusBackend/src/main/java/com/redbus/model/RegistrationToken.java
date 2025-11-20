20eef217213cce97ba610988d2d4190398564693
Sahil inamdar <sahilinamdar212@gmail.com>
2025-11-20 16:21:32 +0530

    Update RegistrationToken.java

--- LinkedBusBackend/src/main/java/com/redbus/model/RegistrationToken.java
+++ LinkedBusBackend/src/main/java/com/redbus/model/RegistrationToken.java
@@ -20,7 +20,7 @@ public class RegistrationToken {
 
     private String email;
     private String name;
-    private String password;  // temporary password until verified
+    private String password; 
     private String phoneNumber;
     private String gender;
     private String city;
